2b7a89b544840994330e0f85c229c9a6defede0f
System <none>
2017-03-24 10:34:21 +0000

    🚩😚 Updated with Glitch

--- public/javascript/auth.js
+++ public/javascript/auth.js
@@ -2,7 +2,7 @@
 // at {{ https://cloud.google.com/console }}.
 // If you run this code from a server other than http://localhost,
 // you need to register your own client ID.
-var OAUTH2_CLIENT_ID = '__YOUR_CLIENT_ID__';
+var OAUTH2_CLIENT_ID = '367177201592-49i42tkiugohglnaia2dvifkceno9bud.apps.googleusercontent.com';
 var OAUTH2_SCOPES = [
   'https://www.googleapis.com/auth/youtube'
 ];
